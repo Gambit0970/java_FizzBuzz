75d981ba4e811e0dc960c1c58be5c19dacfc2512
Thomas Crabtree <thomas.crabtree@cqe.co.uk>
2023-03-06 10:43:15 +0000

    initial commit

---
+++ src/main/java/Game.java
@@ -0,0 +1,19 @@
+public class Game {
+
+    public void playGame (User inGameUser) {
+        for (int n = 0; n <= inGameUser.getCounter(); n++) {
+            if (n % 3 == 0 && n % 5 != 0) {
+                // print Fizz
+                System.out.println("Fizz");
+            } else if (n % 3 != 0 && n % 5 == 0) {
+                // print Buzz
+                System.out.println("Buzz");
+            } else if (n % 3 == 0 && n % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println(n);
+            }
+        }
+    }
+
+}
